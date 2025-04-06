20e928ca07a6c970cdd9ff75988c8fa597629722
Tahoor <tahoormalik4@gmail.com>
2025-04-06 13:09:29 +0500

    Prepare Laravel for Vercel

---
+++ dist/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My App</title>
+    <link rel="stylesheet" href="/assets/app-XXXXXX.css">
+</head>
+<body>
+    <div id="app"></div>
+    <script src="/assets/app-XXXXXX.js"></script>
+</body>
+</html>
